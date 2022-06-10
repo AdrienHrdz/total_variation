830ee44a503e55c244fc91704d99ee055c7b9d1f
AdrienHrdz <adrienhernandez16@gmail.com>
2022-06-10 10:26:05 +0200

    operateur à tester

--- src/operators/opD.py
+++ src/operators/opD.py
@@ -1,2 +1,20 @@
 import scipy.signal as signal
 import numpy as np
+
+def D(x):
+    dim = (np.min(np.shape(x)) > 1) + 1
+    if dim == 1:
+        ker = np.array([-1, 0, 1], dtype=np.float64)
+        grad = signal.convolve(x, ker, mode='same')
+    elif dim == 2:
+        scharr = np.array([[ -3-3j, 0-10j,  +3 -3j],
+                           [-10+0j, 0+ 0j, +10 +0j],
+                           [ -3+3j, 0+10j,  +3 +3j]]) # Gx + j*Gy
+        grad = signal.convolve2d(x, scharr, mode='same', boundary='symm')
+    return grad
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- src/operators/opL.py
+++ src/operators/opL.py
@@ -1,2 +1,20 @@
 import scipy.signal as signal
 import numpy as np
+
+def opL(x):
+    dim = (np.min(np.shape(x)) > 1) + 1
+    if dim == 1:
+        ker = np.array([1, -2, 1], dtype=np.float64)
+        lap = signal.convolve(x, ker, mode='same')
+        pass
+    elif dim == 2:
+        ker = np.array([[0,1,0], [1,-4,1], [0,1,0]], dtype=np.float64) # V4
+        # ker = np.array([[1,1,1], [1,-8,1], [1,1,1]], dtype=np.float64) # V8
+        lap = signal.convolve2d(x, ker, mode='same', boundary='symm')
+    return lap
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
