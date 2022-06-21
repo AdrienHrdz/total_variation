785b52b41de88b0f21f9b51432e6d766b81eadc4
AdrienHrdz <adrienhernandez16@gmail.com>
2022-06-21 13:56:17 +0200

    test

--- src/operators/opD.py
+++ src/operators/opD.py
@@ -1,11 +1,14 @@
 import scipy.signal as signal
 import numpy as np
+import matplotlib.pyplot as plt
 
 def D(x):
-    dim = (np.min(np.shape(x)) > 1) + 1
+    dim = (np.min(np.shape(x)) > 1) #+ 1
     if dim == 1:
-        ker = np.array([-1, 0, 1], dtype=np.float64)
-        grad = signal.convolve(x, ker, mode='same')
+        # ker = np.array([.5, 0, -.5], dtype=np.float64)
+        # ker = ker / np.linalg.norm(ker)
+        # grad = signal.convolve(x, ker, mode='same')
+        grad = np.gradient(x)
     elif dim == 2:
         scharr = np.array([[ -3-3j, 0-10j,  +3 -3j],
                            [-10+0j, 0+ 0j, +10 +0j],
@@ -14,7 +17,28 @@ def D(x):
     return grad
 
 def main():
+    # x = np.linspace(-3.14, 3.14, 100)
+    # y = np.sin(x)
+    # Dy = np.gradient(y)
+
+    # plt.plot(x, y, 'b-', label='y')
+    # plt.plot(x, Dy, 'r-', label='D(y)')
+    
+    # plt.grid()
+    # plt.legend()
+    # plt.show()
+
     pass
 
 if __name__ == '__main__':
-    main()
+    # main()
+    x = np.linspace(-3.14, 3.14, 100)
+    y = np.sin(x)
+    Dy = np.gradient(y)
+
+    plt.plot(x, y, 'b-', label='y')
+    plt.plot(x, Dy, 'r-', label='D(y)')
+    
+    plt.grid()
+    plt.legend()
+    plt.show()
